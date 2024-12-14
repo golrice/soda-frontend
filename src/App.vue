efd8355f5dac8fda413581c045b61c4195025588
golrice <791554926@qq.com>
2024-12-14 11:37:19 +0800

    init proj

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <body class="flex items-center justify-center h-screen">
+    <div class="p-4 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div>
+        <div class="text-xl font-medium text-black">Soda</div>
+        <p class="text-gray-500">Hello world</p>
+      </div>
+      <div class="p-4 bg-gray-200 rounded-full">
+      </div>
+    </div>
+  </body> 
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+</style>
