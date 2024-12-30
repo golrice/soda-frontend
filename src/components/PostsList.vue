73972bef6460d14d003c98f8e76ecfc5a17095c1
Legend717 <154345507+Legend717@users.noreply.github.com>
2024-12-30 18:59:20 +0800

    Cxp (#4)
    * feat(PostsList): 添加用户认证信息到请求头部以支持个性化推荐
    
    * style(ui): 调整卡片和列表的高度样式

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen flex flex-col gap-4">
+  <div class="h-full flex flex-col gap-4">
     <PostDetailsCard v-for="file in files" :key="file.title" :file="file" @click="fetchPost(file.title)" />
   </div>
 </template>
@@ -15,7 +15,21 @@ const files = ref([])
 
 async function fetchPosts() {
   try {
-    const response = await axios.get(`${API_BASE_URL}/get-posts`);
+    let username = "";
+    try {
+      // const token = localStorage.getItem('token');
+      username = localStorage.getItem('username');
+    } catch (error) {
+      alert("请登录以解锁享受个性化推荐！");
+    }
+
+    // 设置axios的请求头部，包含认证token
+    const config = {
+      headers: {
+        ...(username ? { Authorization: `Bearer ${username}` } : {}) // 使用Bearer schema，理论上这玩意用来设计token的，但我用来弄username的也行
+      }
+    };
+    const response = await axios.get(`${API_BASE_URL}/get-posts`, config);
     files.value = response.data["posts"];
   } catch (error) {
     alert("Error with fetching files");
@@ -24,7 +38,22 @@ async function fetchPosts() {
 
 async function fetchPost(name) {
   try {
-    const response = await axios.get(`${API_BASE_URL}/get-post/${name}`);
+    // const token = ''
+    let username = "";
+    try {
+      // const token = localStorage.getItem('token');
+      username = localStorage.getItem('username');
+    } catch (error) {
+      alert("请登录以解锁享受个性化推荐！");
+    }
+
+    // 设置axios的请求头部，包含认证token
+    const config = {
+      headers: {
+        ...(username ? { Authorization: `Bearer ${username}` } : {}) // 使用Bearer schema，理论上这玩意用来设计token的，但我用来弄username的也行
+      }
+    };
+    const response = await axios.get(`${API_BASE_URL}/get-post/${name}`, config);
     const postData = response.data;
 
     router.push({ path: `/posts/${name}`, query: { title: postData['title'], content: postData['content'], author: postData['author'] } })
